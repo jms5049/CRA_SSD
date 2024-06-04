f78caab4c2264a090f19c79fc4de2c9ce98a5058
DESKTOP-5FBE16T\User <haebin1223.myk@gmail.com>
2024-06-04 11:52:08 +0900

    [Delete] Delete pch.h/pch.cpp and modoify to EXPECT_THAT

--- SSD_Test/test.cpp
+++ SSD_Test/test.cpp
@@ -1,8 +1,7 @@
-#include "pch.h"
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+using namespace testing;
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	EXPECT_THAT(1, Eq(1));
 }
