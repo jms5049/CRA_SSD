8a16bdb5a3ee9b140ee32254a90ad20b48e54703
DESKTOP-5FBE16T\User <haebin1223.myk@gmail.com>
2024-06-04 15:06:16 +0900

    -Add 1 digit, 2 digit case

--- SSD/SSD_Test/test.cpp
+++ SSD/SSD_Test/test.cpp
@@ -24,6 +24,26 @@ TEST(ReadTest, validinputtest4) {
 }
 
 TEST(ReadTest, validinputtest5) {
+	string input = "read $";
+	EXPECT_THROW(printResult(input), std::invalid_argument);
+}
+
+TEST(ReadTest, validinputtest6) {
+	string input = "read 2*";
+	EXPECT_THROW(printResult(input), std::invalid_argument);
+}
+
+TEST(ReadTest, validinputtest7) {
 	string input = "read 23";
 	EXPECT_THAT(readIndex(input), 23);
+}
+
+TEST(ReadTest, validinputtest8) {
+	string input = "read 7";
+	EXPECT_THAT(readIndex(input), 7);
+}
+
+TEST(ReadTest, validinputtest9) {
+	string input = "read  4";
+	EXPECT_THROW(printResult(input), std::invalid_argument);
 }
