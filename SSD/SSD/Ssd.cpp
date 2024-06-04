f03b8dafb917bbdb8178083bb0e018208816c9ff
donghoonyeo <www1w1w11@gmail.com>
2024-06-04 15:46:02 +0900

    [refactor] 1. TearDown 부분 함수 구현으로 변경, 2. file read/write 부분 private로 변경하여 Invalid file check는 윗단에서 구현하는것으로 변경 -> invalid test도 추후 다른 방식으로 구현예정 3. write함수에서 input string이  "0x"부분을 추가하는 것으로 변경

--- SSD/SSD/Ssd.cpp
+++ SSD/SSD/Ssd.cpp
@@ -9,11 +9,52 @@ public:
 		return ErrorMessage;
 	}
 
-	string readNandData(string fileName) {
-		ifstream file(fileName);
+	void initializeResourceFile() {
+		ofstream nandFile(nandFlieName, ios::trunc);
+		ofstream resultFile(resultFileName, ios::trunc);
 
+		string nandInit = "";
+		for (int i = 0; i < 800; i++) {
+			nandInit += "0";
+		}
+		nandFile << nandInit;
+		resultFile << "";
+
+		nandFile.close();
+		resultFile.close();
+	}
+
+	string readSsd(int LBAIndex) {
+		string nandData = readNandData();
+
+		if (nandData == ErrorMessage)  return ErrorMessage;
+
+		int startIndex = LBAIndex * 4 * 2;
+		string result = "0x" + nandData.substr(startIndex, 8);
+		updateReadResult(result);
+		return result;
+	}
+
+	void writeSsd(int LBAIndex, string writeData) {
+		string nandData = readNandData();
+
+		if (nandData == ErrorMessage)  return;
+
+		int startIndex = LBAIndex * 4 * 2;
+		for (int i = 0; i < 8; i++) {
+			nandData[startIndex + i] = writeData[2 + i];
+		}
+		writeNandData(nandData);
+	}
+private:
+	string nandFlieName = "../nand.txt";
+	string resultFileName = "../result.txt";
+	string ErrorMessage = "Nand file open error!";
+
+	string readNandData() {
+		ifstream file(nandFlieName);
 		if (file.is_open() == false)  return ErrorMessage;
-		
+
 		string line;
 		getline(file, line);
 
@@ -21,36 +62,19 @@ public:
 		return line;
 	}
 
-	void writeNandData(string fileName, string changedNandData) {
-		ofstream file(fileName);
-
+	void writeNandData(string changedNandData) {
+		ofstream file(nandFlieName);
 		if (file.is_open() == false)  return;
 		file << changedNandData;
 
 		file.close();
 	}
 
-	string readSsd(int LBAIndex) {
-		string nandData = readNandData(nandFlieName);
+	void updateReadResult(string readData) {
+		ofstream file(resultFileName, ios::trunc);
+		if (file.is_open() == false)  return;
+		file << readData;
 
-		if (nandData == ErrorMessage)  return ErrorMessage;
-
-		int startIndex = LBAIndex * 4 * 2;
-		return "0x" + nandData.substr(startIndex, 8);
+		file.close();
 	}
-
-	void writeSsd(int LBAIndex, string writeData) {
-		string nandData = readNandData(nandFlieName);
-
-		if (nandData == ErrorMessage)  return;
-
-		int startIndex = LBAIndex * 4 * 2;
-		for (int i = 0; i < 8; i++) {
-			nandData[startIndex + i] = writeData[i];
-		}
-		writeNandData(nandFlieName, nandData);
-	}
-private:
-	string nandFlieName = "../nand.txt";
-	string ErrorMessage = "Nand file open error!";
 };
--- SSD/SSD_Test/Ssd_test.cpp
+++ SSD/SSD_Test/Ssd_test.cpp
@@ -6,33 +6,22 @@ using namespace testing;
 class SsdVirtualFixture : public Test {
 protected:
 	void TearDown() override {
-		for(int i = 0; i < 100; i++)
-			device.writeSsd(i, "00000000");
+		device.initializeResourceFile();
 	}
 public:
 	Ssd device;
-	string validFileName = "../nand.txt";
-	string invalidFileName = "../flash.txt";
 };
-
-TEST_F(SsdVirtualFixture, readNandDataFileNameError) {
-	EXPECT_THAT(device.readNandData(invalidFileName), Eq(device.getErrorMessage()));
-}
-
-TEST_F(SsdVirtualFixture, readNandDataNandSizeError) {
-	EXPECT_THAT(device.readNandData(validFileName).size(), Eq(800));
-}
 
 TEST_F(SsdVirtualFixture, readNandDataIndex0) {
 	EXPECT_THAT(device.readSsd(0), StartsWith("0x00000000"));
 }
 
 TEST_F(SsdVirtualFixture, writeNandDataIndex0) {
-	device.writeSsd(0, "12345678");
+	device.writeSsd(0, "0x12345678");
 	EXPECT_THAT(device.readSsd(0), Eq("0x12345678"));
 }
 
 TEST_F(SsdVirtualFixture, writeNandDataIndex4) {
-	device.writeSsd(4, "BEEF9090");
+	device.writeSsd(4, "0xBEEF9090");
 	EXPECT_THAT(device.readSsd(4), Eq("0xBEEF9090"));
 }
