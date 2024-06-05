d453dc5520145297aca2fc2576ebb6d2d28c5914
ahran_.choi <carokq@gmail.com>
2024-06-05 14:25:23 +0900

    [refactoring] TestShell의 SSD write 기능 refactoring

--- TestShell/TestShell/Ssd.h
+++ TestShell/TestShell/Ssd.h
@@ -5,4 +5,9 @@ class Ssd : public SsdApi {
 public:
 	void write(int lba, std::string data) override;
 	int read(int lba) override;
+
+private:
+	static const LPCSTR SSD_LOCATION;
+
+	std::string convertToWriteCmd(int lba, std::string& data);
 };
