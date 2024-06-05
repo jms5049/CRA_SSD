dbf1eabdcd6a9c0d421d06871606f60c3f1a2008
ahran_.choi <carokq@gmail.com>
2024-06-05 14:06:02 +0900

    [feature] TestShell Ssd write 기능 구현 - SSD.exe 호출하여 nand에 write 확인 - Test Case 추가 후 txt 열어서 정상 동작 확인

--- TestShell/TestShell/Ssd.cpp
+++ TestShell/TestShell/Ssd.cpp
@@ -8,8 +8,9 @@ using std::string;
 void Ssd::write(int lba, std::string data)
 {
 	// ex) 호출부 예시
-	// string writeCmd = lba + " " + data;
-	// ShellExecuteA(NULL, "open", "ssd.exe", writeCmd.c_str(), NULL, SW_SHOW);
+	string writeCmd = "W " + std::to_string(lba);
+	writeCmd += " " + data;
+	ShellExecuteA(NULL, "open", "ssd", writeCmd.c_str(), "..\\..\\SSD\\", SW_HIDE);
 }
 
 int Ssd::read(int lba)
