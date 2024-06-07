e6b8503766303873e417dccf46b6167ee2ba3a0c
DESKTOP-5FBE16T\User <>
2024-06-07 15:00:35 +0900

    [ADD] File Size Checker and Zipper Added

--- TestShell/TestShell/Logger.cpp
+++ TestShell/TestShell/Logger.cpp
@@ -20,21 +20,39 @@ Logger* Logger::logger = nullptr;
     return logger;
 }
 
- void Logger::write(string funcName) {
+ void Logger::write(string* funcName) {
      auto now = chrono::system_clock::now();
      time_t currentTime = chrono::system_clock::to_time_t(now);
      tm* currentTimeStruct = localtime(&currentTime);
 
      cout << "[" << put_time(currentTimeStruct, "%Y-%m-%d %H:%M:%S") << "] ";
-     cout << left << setw(30) << funcName.c_str() << endl;
+     cout << left << setw(30) << funcName->c_str() << endl;
 
      fileWrite(currentTimeStruct, funcName);
  }
 
- void Logger::fileWrite(tm* curTime, string funcName) {
+ void Logger::fileWrite(tm* curTime, string* funcName) {
      ofstream logFile(filePath, std::ios::app);
      if (logFile.is_open() == false)  return;
      logFile << put_time(curTime, "%Y-%m-%d %H:%M:%S") << " ";
-     logFile << left << setw(30) << funcName.c_str() << endl;
+     logFile << left << setw(30) << funcName->c_str() << endl;
      logFile.close();
+ }
+
+ //To-Do :
+ // Check isOverflow after every filewrite excuted and do zipFile if isOverflow returns true;
+ // e.g. if(isOverflow) zipFIle("newFileName") <- newFileName should be stated as is in PPT
+
+ bool Logger::isOverflow() {
+     ifstream logFile("latest.log", ios::binary | ios::ate);
+     int fileSize = logFile.tellg();
+     logFile.close();
+     if (fileSize >= (10*1024)) return true;
+     return false;
+ }
+
+ void Logger::zipFile(string* newFileName) {
+     string newName = newFileName->append(".zip");
+     if(std::rename("latest.log", newName.c_str())) throw std::exception("Error Renaming");
+
  }
