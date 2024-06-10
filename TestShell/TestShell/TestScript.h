36f06e8c53f87530794e8e03ac88b4a1f1878e76
ahran_.choi <ahran_.choi@gmail.com>
2024-06-10 15:25:35 +0900

    [refactoring] TestScript 에서 TestScenario 생성 코드 refactoring

--- TestShell/TestShell/TestScript.h
+++ TestShell/TestShell/TestScript.h
@@ -1,16 +1,21 @@
 #pragma once
 using namespace std;
 
+class TestScenario;
+
 class TestScript {
 public:
 	static TestScript* getInstance();
+	bool isSupportedTestScenario(string testScenarioName);
 	bool testScriptApp(string userInput);
 
 private:
 	static TestScript* testScript;
+	static const vector<string> supportedScenarios;
 
 	TestScript();
 	vector<string> splitString(const string& str);
+	TestScenario* getTestScenario(const string& scenarioName);
 
 	void log(string funcName, string strLog);
 };
