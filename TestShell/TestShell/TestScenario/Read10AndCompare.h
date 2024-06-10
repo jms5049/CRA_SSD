647c29c19bcc0b8a6a82e73db4b8fca2e04a7e20
ahran_.choi <ahran_.choi@gmail.com>
2024-06-10 14:26:26 +0900

    [self review] 불필요한 include 제거

--- TestShell/TestShell/TestScenario/Read10AndCompare.h
+++ TestShell/TestShell/TestScenario/Read10AndCompare.h
@@ -1,11 +1,4 @@
 #pragma once
-#include <string>
-#include "../TestShell.h"
-#include <typeinfo>
-
-#include "TestScenario.h"
-
-using std::string;
 
 class Read10AndCompare : public TestScenario {
 public:
