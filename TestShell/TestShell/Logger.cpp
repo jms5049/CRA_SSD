bceb944641a8f259d760290340f69c485f8fc29b
DESKTOP-5FBE16T\User <>
2024-06-10 09:29:04 +0900

    [Refactor] Avoid Secure Warnings

--- TestShell/TestShell/Logger.cpp
+++ TestShell/TestShell/Logger.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-Logger::Logger() {
+Logger::Logger(){
 }
 
 Logger* Logger::logger = nullptr;
@@ -22,12 +22,13 @@ Logger* Logger::logger = nullptr;
  void Logger::write(string funcName) {
      auto now = chrono::system_clock::now();
      time_t currentTime = chrono::system_clock::to_time_t(now);
-     tm* currentTimeStruct = localtime(&currentTime);
+     tm currentTimeStruct;
+     localtime_s(&currentTimeStruct , &currentTime);
 
-     cout << "[" << put_time(currentTimeStruct, "%Y-%m-%d %H:%M:%S") << "] ";
+     cout << "[" << put_time(&currentTimeStruct, "%Y-%m-%d %H:%M:%S") << "] ";
      cout << left << setw(30) << funcName.c_str() << endl;
 
-     fileWrite(currentTimeStruct, funcName);
+     fileWrite(&currentTimeStruct, funcName);
  }
 
  void Logger::fileWrite(tm* curTime, string funcName) {
--- TestShell/TestShell/Logger.h
+++ TestShell/TestShell/Logger.h
@@ -6,13 +6,9 @@ public:
     void write(std::string funcName);
 
 private:
-    static Logger* logger;
-    std::string filePath;
-
     Logger();
-    Logger& operator=(const Logger& other) = delete;
-    Logger(const Logger& other) = delete;
-
+    static Logger* logger;
+    static string file;
     static void fileWrite(std::tm* curTime, std::string funcName);
     static void zipFile(std::string* newFileName);
     static bool isOverflow();
