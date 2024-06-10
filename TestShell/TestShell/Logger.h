0dac92ab719c80abebcd30fe8b6506db05d45743
min.byun <leaftotoro@gmail.com>
2024-06-10 13:56:30 +0900

    <add> save log file with time and zip

--- TestShell/TestShell/Logger.h
+++ TestShell/TestShell/Logger.h
@@ -12,4 +12,9 @@ private:
     static void fileWrite(std::tm* curTime, std::string funcName, std::string strLog);
     static void zipFile(std::string* newFileName);
     static bool isOverflow();
+    static void saveLogFileWithTime();
+    static std::string getNowFileName();
+    static void checkOverflowAndSaveFile();
+    static void changeFileToZip();
+    static void copyFile(std::string sourceFileName, std::string targetFileName);
 };
